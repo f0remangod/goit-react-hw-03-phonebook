614f39ef9fb6f8faf664d2c6afc959930253bb79
Viktor Biletskyi <106250265+f0remangod@users.noreply.github.com>
2022-12-26 18:37:39 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Feedback } from './FeedbackComponents/Feedback/Feedback';
+import { Phonebook } from './PhonebookComponents/Phonebook/Phonebook';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Feedback />
+      <Phonebook />
+    </>
   );
 };
---
+++ src/components/PhonebookComponents/Phonebook/Phonebook.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import initialValues from '../../../json/initialValues';
+import { Filter } from 'components/PhonebookComponents/Filter/Filter';
+import { ContactsList } from 'components/PhonebookComponents/ContactsList/ContactsList';
+import { PhonebookForm } from 'components/PhonebookComponents/PhonebookForm/PhonebookForm';
+import { Section } from 'components/Section/Section';
+
+export class Phonebook extends React.Component {
+  state = {
+    contacts: initialValues,
+    filter: '',
+  };
+
+  handleFilterChange = event => {
+    const { value } = event.currentTarget;
+    this.setState({
+      filter: value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase().trim();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  clearFilterField = () => {
+    this.setState({ filter: '' });
+  };
+
+  addContact = contact => {
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <>
+        <Section title="Add new contact">
+          <PhonebookForm
+            contacts={contacts}
+            addContact={this.addContact}
+          ></PhonebookForm>
+        </Section>
+
+        <Section title="Filter contacts">
+          <Filter
+            onChange={this.handleFilterChange}
+            value={filter}
+            onClick={this.clearFilterField}
+          ></Filter>
+        </Section>
+
+        <Section title="Saved contacts">
+          <ContactsList
+            filteredContacts={this.getFilteredContacts()}
+            onDelete={this.deleteContact}
+          ></ContactsList>
+        </Section>
+      </>
+    );
+  }
+}
